fb51fae78e315cbc47f68295e4062e04394eb540
StarrFox <starrfox6312@gmail.com>
2023-04-25 02:09:52 -0500

    style(__main__.py): reformat

--- commit_zen_testing/__main__.py
+++ commit_zen_testing/__main__.py
@@ -1,26 +1,12 @@
-"""
-Zen
-"""
-
-
 def my_function(arg1: int):
-    """
-    My function
-    """
     return arg1 + 3
 
 
 def other():
-    """
-    My other function
-    """
     return 2
 
 
 def main():
-    """
-    entry point
-    """
     print("Hello world!")
 
 
